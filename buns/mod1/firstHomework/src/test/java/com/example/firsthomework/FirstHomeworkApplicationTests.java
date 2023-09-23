9f1650b64e21e11943911989977108266a68b0d1
8CloudyCat8 <matveev2464@gmail.com>
2023-09-23 15:09:02 +0500

    Проверка

---
+++ buns/mod1/firstHomework/src/test/java/com/example/firsthomework/FirstHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firsthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
