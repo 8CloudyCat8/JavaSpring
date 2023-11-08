05269bee51cb694cf6b7045755f305351cb771ea
8CloudyCat8 <matveev2464@gmail.com>
2023-11-08 18:30:00 +0500

    ДЗ-8

---
+++ buns/mod8/EighthHomework/src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/EighthHomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.zaurtregulov.spring.springboot.spring_course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EighthHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EighthHomeworkApplication.class, args);
+	}
+}
