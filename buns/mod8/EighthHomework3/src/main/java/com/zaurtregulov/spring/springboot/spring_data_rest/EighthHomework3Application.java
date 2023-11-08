5fd3dbe4c543c70b2c70984c504dfd451173d582
8CloudyCat8 <matveev2464@gmail.com>
2023-11-08 18:52:02 +0500

    8 ДЗ

--- buns/mod8/EighthHomework3/src/main/java/com/zaurtregulov/spring/springboot/spring_data_rest/EighthHomework3Application.java
+++ buns/mod8/EighthHomework3/src/main/java/com/zaurtregulov/spring/springboot/spring_data_rest/EighthHomework3Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EighthHomework3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(EighthHomework3Application.class, args);
     }
-
 }
